95db317c3be1d1503cd87a305b397f0eff0b1f0a
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-12-05 13:01:53 -0700

    fix

--- server.js
+++ server.js
@@ -13,12 +13,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors(
-  {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  }
-));
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -26,9 +21,9 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/games', require('./routes/gameRoutes'));
 
 // Test routes (only in development)
-if (process.env.NODE_ENV === 'development') {
-  app.use('/test', require('./test'));
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use('/test', require('./test'));
+// }
 
 // Error Handler
 app.use(errorHandler);
