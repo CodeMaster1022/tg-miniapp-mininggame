8d1e57117328e3a79d022b12840c13d490b19214
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-12-05 12:33:48 -0700

    fix

--- server.js
+++ server.js
@@ -12,16 +12,13 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-const corsOptions = {
-  origin:['https://new-game-psi.vercel.app/', 'https://t.me'], // Add your production domains
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  maxAge: 86400 // 24 hours
-};
-
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }
+));
 app.use(express.json());
 
 // Routes
